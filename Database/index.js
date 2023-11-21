a73db8820464a19ce1cf8c3911f0ead1f5944fe8
dmitmatv <dmitry.d.matveev@gmail.com>
2023-11-21 06:21:30 -0500

    revert

--- Database/index.js
+++ Database/index.js
@@ -1,17 +1,17 @@
-/*import courses from "./courses.json" assert { type: "json" };
+import courses from "./courses.json" assert { type: "json" };
 import modules from "./modules.json" assert { type: "json" };
 import assignments from "./assignments.json" assert { type: "json" };
 import users from "./users.json" assert { type: "json" };
 import grades from "./grades.json" assert { type: "json" };
-import enrollments from "./enrollments.json" assert { type: "json" };*/
+import enrollments from "./enrollments.json" assert { type: "json" };
 
-import { readFileSync } from "fs";
+/*import { readFileSync } from "fs";
 const courses = JSON.parse(readFileSync(".Database/courses.json"));
 const modules = JSON.parse(readFileSync(".Database/modules.json"));
 const assignments = JSON.parse(readFileSync(".Database/assignments.json"));
 const users = JSON.parse(readFileSync(".Database/users.json"));
 const grades = JSON.parse(readFileSync(".Database/grades.json"));
-const enrollments = JSON.parse(readFileSync(".Database/enrollments.json"));
+const enrollments = JSON.parse(readFileSync(".Database/enrollments.json"));*/
 
 
 export default {
