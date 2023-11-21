6bb2f0f94a1b749913e428ca289dedc9fbcefb59
dmitmatv <dmitry.d.matveev@gmail.com>
2023-11-21 13:07:09 -0500

    type module in json

--- Database/index.js
+++ Database/index.js
@@ -1,10 +1,10 @@
 //import courses from "./courses.json" assert { type: "json" };
-import {modules} from "./modules";
-import {assignments} from "./assignments"
-import {users} from "./users"
-import {grades} from "./grades"
-import {enrollments} from "./enrollments"
-import {courses} from "./courses"
+//import {modules} from "./modules";
+//import {assignments} from "./assignments"
+//import {users} from "./users"
+//import {grades} from "./grades"
+//import {enrollments} from "./enrollments"
+import {courses} from "./courses";
 
 /*import { readFileSync } from "fs";
 const courses = JSON.parse(readFileSync(".Database/courses.json"));
