ea012565717650d4d337f3c75b07f47bc1e58e9a
dmitmatv <dmitry.d.matveev@gmail.com>
2023-12-04 21:35:55 -0500

    a6 done

--- app.js
+++ app.js
@@ -4,14 +4,41 @@ import Lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
 import "dotenv/config";
-
+import session from "express-session";
+import "dotenv/config";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(cors());
+app.use(cors({
+                 credentials: true,
+                 origin: process.env.FRONTEND_URL,
+             }
+));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(
+    session(sessionOptions)
+);
+
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 
 Hello(app);
 Lab5(app);
